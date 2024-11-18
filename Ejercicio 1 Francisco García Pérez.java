eecff176d37c94edb65fb3bf76258004ef2ee0ba
Francisco García Pérez <franciscogarciaperez2002@gmail.com>
2024-11-18 18:11:54 +0100

    Add files via upload
    Francisco García Pérez

---
+++ Ejercicio 1 Francisco García Pérez.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Ejercicio1 {
+    public static void main(String[] args) {
+System.out.println("Bienvenido a entorno de desarrollo donde vas a aprender a programar Java");
+        System.out.println(3+4);
+    }
+}
